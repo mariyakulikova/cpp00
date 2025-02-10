34a2319465485464604cb116a9bbe2bb91d4ef7f
Mariia Kulikova <mkulikov@student.42berlin.de>
2025-02-10 13:40:33 +0100

    add ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 13:18:26 by mkulikov          #+#    #+#             */
+/*   Updated: 2025/02/10 13:39:24 by mkulikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; j < argv[i][j]; j++)
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
