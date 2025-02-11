2fa0eabe4f28200b94f8b0ee86a2e1eda968e528
mkulikov <mkulikov@student.42berlin.de>
2025-02-11 15:11:29 +0100

    add using statment

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:18:26 by mkulikov          #+#    #+#             */
-/*   Updated: 2025/02/10 16:45:04 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:10:56 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,24 @@
 #include <cctype>
 #include <string>
 
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			std::string str(argv[i]);
-			for (size_t j = 0; j < str.length(); j++)
-			{
-				std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[j])));
-			}
-			if (i < argc - 1)
-			{
-				std::cout << " ";
+using std::cout;
+using std::endl;
+using std::string;
+using std::toupper;
+
+
+int	main(int argc, char **argv) {
+	if (argc == 1) {
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			string str(argv[i]);
+			for (size_t j = 0; j < str.length(); j++) {
+				cout << static_cast<char>(toupper(static_cast<unsigned char>(str[j])));
 			}
 		}
-		std::cout << std::endl;
+		cout << endl;
 	}
 	return (0);
 }
